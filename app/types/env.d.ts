944483cce14e7c73562258e575834242847ba008
yg1110 <younggil9488@gmail.com>
2024-07-15 19:43:08 +0900

    Add env

---
+++ app/types/env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+  export const NAVER_KEY: string;
+  export const NAVER_SECRET: string;
+  export const NAVER_APP_NAME: string;
+  export const NAVER_SERVICE_URL_SCHEME: string;
+}
